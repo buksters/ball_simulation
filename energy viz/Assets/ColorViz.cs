951cbdfa06102046c7cc4fcf2953d5d6d5e34ef6
buksters <ibukiwasaki@gmail.com>
2022-04-21 00:03:50 -0400

    added halo, changed environment

--- energy viz/Assets/ColorViz.cs
+++ energy viz/Assets/ColorViz.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class ColorViz : MonoBehaviour
 {
     public Text txt;
     Renderer sphereRenderer;
     private Color newSphereColor;
+    private Color newHaloColor;
     private float potentialEnergy, kineticEnergy;
     private float green, blue;
     private float totalEnergy;
+    private float originalEnergy;
+    private float mass;
+    
+
 
     //map(value, oldlow, oldhigh, newlow, newhigh)
     float map(float s, float a1, float a2, float b1, float b2)
@@ -22,12 +28,16 @@ public class ColorViz : MonoBehaviour
     {
         sphereRenderer = GetComponent<Renderer>();
         txt = GetComponent<UnityEngine.UI.Text>();
+        mass =  GetComponent<Rigidbody>().mass;
+        originalEnergy = -mass*Physics.gravity.y*transform.position.y;
     }
 
     void FixedUpdate()
     {
-        float mass =  GetComponent<Rigidbody>().mass;
+
+        // float mass =  GetComponent<Rigidbody>().mass;
         Vector3 vel = GetComponent<Rigidbody>().velocity;
+        float Height = transform.position.y;
         potentialEnergy = -mass*Physics.gravity.y*transform.position.y;
         kineticEnergy = 0.5f*mass*(vel.y*vel.y + vel.x*vel.x);
         totalEnergy = potentialEnergy+kineticEnergy;
@@ -35,8 +45,21 @@ public class ColorViz : MonoBehaviour
         blue = map(kineticEnergy, 0f, totalEnergy, 0f, 1f);
         // txt.text = totalEnergy.ToString();
 
-        newSphereColor = new Color(0f, green, blue, 1f);
+        newSphereColor = new Color(0f, green, blue, .5f);
+        newHaloColor = new Color(0f, green, blue, 1f);
 
         sphereRenderer.material.SetColor("_Color", newSphereColor);
+        
+        // transform.localScale = new Vector3 (1.0001f, 1.0001f, 1.0001f); //changes size of ball
+
+        // to represent energy loss
+        float energyDelta = totalEnergy/originalEnergy;
+
+        // change halo properties:
+        SerializedObject halo = new SerializedObject(GetComponent("Halo"));
+        halo.FindProperty("m_Size").floatValue = 2f*energyDelta;
+        halo.FindProperty("m_Enabled").boolValue = true;
+        halo.FindProperty("m_Color").colorValue = newHaloColor;
+        halo.ApplyModifiedProperties();
     }
 }
